bb5bb6579dc301347a948cf702cd3cc77d495823
Jessica Diver <jessica.diver@painchek.com>
2019-07-16 14:39:46 +1000

    first commit on project-3

---
+++ src/store/modules/stocks.js
@@ -0,0 +1,39 @@
+import stocks from '../../data/stocks';
+
+const state = {
+    stocks: []
+};
+
+const mutations = {
+    'SET_STOCKS'(state, stocks) {
+        state.stocks = stocks;
+    },
+    'RDM_STOCKS'(state) {
+
+    }
+};
+
+const actions = {
+    buyStock: ({ commit }, order) => {
+        commit('BUY_STOCK', order);
+    },
+    initStocks: ({commit}) => {
+        commit('SET_STOCKS', stocks);
+    },
+    randomizeStocks: ({commit}) => {
+        commit('RDM_STOCKS');
+    }
+};
+
+const getters = {
+    stocks: state => {
+        return state.stocks;
+    }
+};
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+}
